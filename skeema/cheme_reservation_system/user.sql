ac74d8a88d054ecdb5df625d21630031fb4c1c74
tarao1006 <taiga4562@gmail.com>
2021-04-03 02:10:04 +0900

    Add back quote

--- skeema/cheme_reservation_system/user.sql
+++ skeema/cheme_reservation_system/user.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS user (
-  id VARCHAR(255) NOT NULL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  name_ruby VARCHAR(255) NOT NULL,
-  password_digest VARCHAR(255) NOT NULL,
-  email_address VARCHAR(255) NOT NULL
+  `id` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `name_ruby` VARCHAR(255) NOT NULL,
+  `password_digest` VARCHAR(255) NOT NULL,
+  `email_address` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
 );
