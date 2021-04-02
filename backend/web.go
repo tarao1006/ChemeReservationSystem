ef3cb2117d8d62c2576f930038411d9f3d80fdc8
tarao1006 <taiga4562@gmail.com>
2021-04-02 13:10:21 +0900

    init backend

---
+++ backend/web.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
