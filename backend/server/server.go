83ec99eacb49a18e9bf7e4cbfa165c64cbb9f7a5
tarao1006 <taiga4562@gmail.com>
2021-04-03 12:47:38 +0900

    Add server

---
+++ backend/server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tarao1006/ChemeReservationSystem/db"
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	db     *gorm.DB
+	router *gin.Engine
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Init(dsn string) error {
+	db, err := db.Open(dsn)
+	if err != nil {
+		return fmt.Errorf("failed init db. %s", err)
+	}
+	s.db = db
+	s.router = s.Route()
+	return nil
+}
+
+func (s *Server) Route() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return r
+}
