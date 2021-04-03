2024a859e9d502dcdc17bbc9b24da44880b082c5
tarao1006 <taiga4562@gmail.com>
2021-04-03 19:34:43 +0900

    Add User model

---
+++ backend/model/user.go
@@ -0,0 +1,9 @@
+package model
+
+type User struct {
+	ID             int    `db:"id" json:"id"`
+	Name           string `db:"name" json:"name"`
+	NameRuby       string `db:"name_ruby" json:"name_ruby"`
+	PasswordDigest string `db:"password_digest" json:"password_digest"`
+	EmailAddress   string `db:"email_address" json:"email_address"`
+}
