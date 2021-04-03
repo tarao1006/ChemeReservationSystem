d265db5e75df2e78beccc7d45f1a5d81844cd17b
tarao1006 <taiga4562@gmail.com>
2021-04-03 20:46:56 +0900

    Add binding

--- backend/model/user.go
+++ backend/model/user.go
@@ -5,9 +5,9 @@ func (User) TableName() string {
 }
 
 type User struct {
-	ID             string `db:"id" json:"id"`
-	Name           string `db:"name" json:"name"`
-	NameRuby       string `db:"name_ruby" json:"name_ruby"`
-	PasswordDigest string `db:"password_digest" json:"password_digest"`
-	EmailAddress   string `db:"email_address" json:"email_address"`
+	ID             string `db:"id" json:"id" binding:"required"`
+	Name           string `db:"name" json:"name" binding:"required"`
+	NameRuby       string `db:"name_ruby" json:"name_ruby" binding:"required"`
+	PasswordDigest string `db:"password_digest" json:"password_digest" binding:"required"`
+	EmailAddress   string `db:"email_address" json:"email_address" binding:"required"`
 }
