0b5eabbdff5822a429bbf916d47706e4051f5a1e
tarao1006 <taiga4562@gmail.com>
2021-04-04 00:28:47 +0900

    Update user service

--- backend/service/user.go
+++ backend/service/user.go
@@ -1,8 +1,6 @@
 package service
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/tarao1006/ChemeReservationSystem/db"
 	"github.com/tarao1006/ChemeReservationSystem/model"
@@ -69,13 +67,8 @@ func (UserService) UpdateByID(id string, c *gin.Context) (model.User, error) {
 
 func (UserService) DeleteByID(id string) error {
 	db := db.GetDB()
-	res := db.Where("id = ?", id)
-
-	fmt.Println(res.Error)
-
 	if err := db.Where("id = ?", id).Delete(&model.User{}).Error; err != nil {
 		return err
 	}
-
 	return nil
 }
