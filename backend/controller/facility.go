98db41c0e77c76900ea1143b6109f85552373086
tarao1006 <taiga4562@gmail.com>
2021-04-04 00:20:49 +0900

    Update facility controller

--- backend/controller/facility.go
+++ backend/controller/facility.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,55 +15,47 @@ func NewFacilityController() *FacilityController {
 
 func (FacilityController) Index(c *gin.Context) {
 	s := service.NewFacilityService()
-
 	facilities, err := s.GetAll()
 
 	if err != nil {
-		c.AbortWithStatus(http.StatusNotFound)
-		fmt.Println(err)
-	} else {
-		c.JSON(http.StatusOK, facilities)
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
 	}
+	c.JSON(http.StatusOK, facilities)
 }
 
 func (FacilityController) Create(c *gin.Context) {
 	s := service.NewFacilityService()
-
 	f, err := s.Create(c)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
 	c.JSON(http.StatusOK, f)
 }
 
 func (FacilityController) Show(c *gin.Context) {
 	s := service.NewFacilityService()
 	id := c.Params.ByName("id")
-
 	f, err := s.GetByID(id)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
 	c.JSON(http.StatusOK, f)
 }
 
 func (FacilityController) Update(c *gin.Context) {
 	s := service.NewFacilityService()
 	id := c.Params.ByName("id")
-
 	f, err := s.UpdateByID(id, c)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
 	c.JSON(http.StatusOK, f)
 }
 
@@ -76,6 +67,5 @@ func (FacilityController) Delete(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
