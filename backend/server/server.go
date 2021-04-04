f3b3e90f3feb5f9cd1a2f221b0b26286acf43194
tarao1006 <taiga4562@gmail.com>
2021-04-04 11:11:49 +0900

    Update routing

--- backend/server/server.go
+++ backend/server/server.go
@@ -35,35 +35,35 @@ func (s *Server) Run(port int) {
 func router() *gin.Engine {
 	r := gin.Default()
 
-	u := r.Group("/user")
-	{
-		c := controller.NewUserController()
-		u.GET("", c.Index)
-		u.GET("/:id", c.Show)
-		u.POST("", c.Create)
-		u.PUT("/:id", c.Update)
-		u.DELETE("/:id", c.Delete)
-	}
+	// u := r.Group("/user")
+	// {
+	// 	c := controller.NewUserController()
+	// 	u.GET("", c.Index)
+	// 	u.GET("/:id", c.Show)
+	// 	u.POST("", c.Create)
+	// 	u.PUT("/:id", c.Update)
+	// 	u.DELETE("/:id", c.Delete)
+	// }
 
-	ut := r.Group("/user_type")
-	{
-		c := controller.NewUserTypeController()
-		ut.GET("", c.Index)
-		ut.GET("/:id", c.Show)
-		ut.POST("", c.Create)
-		ut.PUT("/:id", c.Update)
-		ut.DELETE("/:id", c.Delete)
-	}
+	// ut := r.Group("/user_type")
+	// {
+	// 	c := controller.NewUserTypeController()
+	// 	ut.GET("", c.Index)
+	// 	ut.GET("/:id", c.Show)
+	// 	ut.POST("", c.Create)
+	// 	ut.PUT("/:id", c.Update)
+	// 	ut.DELETE("/:id", c.Delete)
+	// }
 
-	f := r.Group("/facility")
-	{
-		c := controller.NewFacilityController()
-		f.GET("", c.Index)
-		f.GET("/:id", c.Show)
-		f.POST("", c.Create)
-		f.PUT("/:id", c.Update)
-		f.DELETE("/:id", c.Delete)
-	}
+	// f := r.Group("/facility")
+	// {
+	// 	c := controller.NewFacilityController()
+	// 	f.GET("", c.Index)
+	// 	f.GET("/:id", c.Show)
+	// 	f.POST("", c.Create)
+	// 	f.PUT("/:id", c.Update)
+	// 	f.DELETE("/:id", c.Delete)
+	// }
 
 	ft := r.Group("/facility_type")
 	{
@@ -75,15 +75,15 @@ func router() *gin.Engine {
 		ft.DELETE("/:id", c.Delete)
 	}
 
-	p := r.Group("/plan")
-	{
-		c := controller.NewFacilityTypeController()
-		p.GET("", c.Index)
-		p.GET("/:id", c.Show)
-		p.POST("", c.Create)
-		p.PUT("/:id", c.Update)
-		p.DELETE("/:id", c.Delete)
-	}
+	// p := r.Group("/plan")
+	// {
+	// 	c := controller.NewFacilityTypeController()
+	// 	p.GET("", c.Index)
+	// 	p.GET("/:id", c.Show)
+	// 	p.POST("", c.Create)
+	// 	p.PUT("/:id", c.Update)
+	// 	p.DELETE("/:id", c.Delete)
+	// }
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
