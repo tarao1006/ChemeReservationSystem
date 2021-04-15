7a2c75f09d74bc30e8fa4ea54e580a4551d165ac
tarao1006 <taiga4562@gmail.com>
2021-04-16 02:07:57 +0900

    Add plan_test

---
+++ backend/repository/plan_test.go
@@ -0,0 +1,86 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/tarao1006/ChemeReservationSystem/db"
+)
+
+func TestShouldGetAllPlans(t *testing.T) {
+	r := NewPlanRepository()
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+
+	rows := sqlmock.NewRows([]string{"id", "name"}).
+		AddRow(1, "new_plan")
+
+	mock.ExpectQuery("SELECT (.*) FROM plan").WillReturnRows(rows)
+
+	if _, err := r.GetAll(sqlxDB); err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestShouldGetPlanByID(t *testing.T) {
+	r := NewPlanRepository()
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+
+	rows := sqlmock.NewRows([]string{"id", "name"}).
+		AddRow(1, "new_plan")
+
+	mock.ExpectQuery("SELECT (.*) FROM plan WHERE id = ?").WithArgs(1).WillReturnRows(rows)
+
+	_, err = r.FindByID(sqlxDB, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestShouldCreatePlan(t *testing.T) {
+	r := NewPlanRepository()
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+
+	mock.ExpectBegin()
+	mock.ExpectExec("INSERT INTO plan").WithArgs("plan_001").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	if err := db.TXHandler(sqlxDB, func(tx *sqlx.Tx) error {
+		if _, err := r.Create(tx, "plan_001"); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
