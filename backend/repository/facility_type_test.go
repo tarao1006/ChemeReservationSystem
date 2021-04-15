b5a85e5af459a486180948921a850bb5a523bbe7
tarao1006 <taiga4562@gmail.com>
2021-04-16 00:36:00 +0900

    Remove ByIDs

--- backend/repository/facility_type_test.go
+++ backend/repository/facility_type_test.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -48,32 +47,6 @@ func TestGetFacilityTypeByID(t *testing.T) {
 	mock.ExpectQuery("SELECT (.*) FROM facility_type WHERE id = ?").WithArgs(1).WillReturnRows(rows)
 
 	_, err = r.FindByID(sqlxDB, 1)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("there were unfulfilled expectations: %s", err)
-	}
-}
-
-func TestGetFacilityTypesByIDs(t *testing.T) {
-	r := NewFacilityTypeRepository()
-	mockDB, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer mockDB.Close()
-	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
-
-	rows := sqlmock.NewRows([]string{"id", "name"}).
-		AddRow(1, "new_facility_type_1").
-		AddRow(2, "new_facility_type_2")
-
-	mock.ExpectQuery("SELECT (.*) FROM facility_type WHERE id in (?)").WithArgs([]int64{1, 2}).WillReturnRows(rows)
-
-	fts, err := r.FindByIDs(sqlxDB, []int64{1, 2})
-	fmt.Println(fts)
 	if err != nil {
 		t.Error(err)
 	}
