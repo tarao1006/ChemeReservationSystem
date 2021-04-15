7cc1e3adf799491fa3daa9edfca00efa0a4a9992
tarao1006 <taiga4562@gmail.com>
2021-04-16 01:57:23 +0900

    Add TestShouldCreateFacilityType

--- backend/repository/facility_type_test.go
+++ backend/repository/facility_type_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jmoiron/sqlx"
+	"github.com/tarao1006/ChemeReservationSystem/db"
 )
 
 func TestGetAllFacilityTypes(t *testing.T) {
@@ -48,6 +49,34 @@ func TestGetFacilityTypeByID(t *testing.T) {
 
 	_, err = r.FindByID(sqlxDB, 1)
 	if err != nil {
+		t.Error(err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestShouldCreateFacilityType(t *testing.T) {
+	r := NewFacilityTypeRepository()
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockDB.Close()
+
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+
+	mock.ExpectBegin()
+	mock.ExpectExec("INSERT INTO facility_type").WithArgs("facility_type_001").WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	if err := db.TXHandler(sqlxDB, func(tx *sqlx.Tx) error {
+		if _, err := r.Create(tx, "facility_type_001"); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
 		t.Error(err)
 	}
 
