6a315a8e0cd5fbb688e81f2603f9cf3f2bf51457
tarao1006 <taiga4562@gmail.com>
2021-04-25 12:45:20 +0900

    Update auth context

--- frontend/src/contexts/auth.tsx
+++ frontend/src/contexts/auth.tsx
@@ -1,42 +1,28 @@
 import React, { createContext, useState } from "react"
-import { User, getMe, login as loginAPI, logout as logoutAPI } from '@api'
+import { User } from '@api'
 
 type user = User | undefined
 
 interface IAuthContext {
   token: string
   currentUser: user
-  login(userId: string, password: string, remember: boolean): void
-  logout(): void
+  setToken: React.Dispatch<React.SetStateAction<string>>
+  setCurrentUser: React.Dispatch<React.SetStateAction<User>>
 }
 
 export const AuthContext = createContext<IAuthContext>({
   token: "",
   currentUser: undefined,
-  login: (userId: string, password: string, remember: boolean) => {},
-  logout: () => {}
+  setToken: () => {},
+  setCurrentUser: () => {}
 })
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState<string>('')
   const [currentUser, setCurrentUser] = useState<user>(undefined)
 
-  const login = async (userId: string, password: string, remember: boolean): Promise<void> => {
-    const jwt = await loginAPI(userId, password, remember)
-    const t = jwt.token
-    const u = await getMe(t)
-    setToken(t)
-    setCurrentUser(u)
-  }
-
-  const logout = async (): Promise<void> => {
-    await logoutAPI()
-    setToken('')
-    setCurrentUser(undefined)
-  }
-
   return (
-    <AuthContext.Provider value={{token, currentUser, login, logout}}>
+    <AuthContext.Provider value={{ token, currentUser, setToken, setCurrentUser }}>
       {children}
     </AuthContext.Provider>
   )
