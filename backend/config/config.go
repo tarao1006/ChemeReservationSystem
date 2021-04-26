69f9ce69d3e8729971ced74aab531bd02e7237f2
tarao1006 <taiga4562@gmail.com>
2021-04-27 00:05:00 +0900

    Add token

--- backend/config/config.go
+++ backend/config/config.go
@@ -5,31 +5,3 @@ import "os"
 func Port() string {
 	return os.Getenv("PORT")
 }
-
-func SecretKeyAccessToken() []byte {
-	return []byte(os.Getenv("SECRET_KEY_ACCESS_TOKEN"))
-}
-
-func SecretKeyRememberMeToken() []byte {
-	return []byte(os.Getenv("SECRET_KEY_REMEMBER_ME_TOKEN"))
-}
-
-func CookieNameAccessToken() string {
-	return os.Getenv("COOKIE_NAME_ACCESS_TOKEN")
-}
-
-func CookieNameRememberMeToken() string {
-	return os.Getenv("COOKIE_NAME_REMEMBER_ME_TOKEN")
-}
-
-func IdentityKeyAccessToken() string {
-	return os.Getenv("KEY_ACCESS_TOKEN")
-}
-
-func IdentityKeyRememberMeToken() string {
-	return os.Getenv("KEY_REMEMBER_ME_TOKEN")
-}
-
-func Realm() string {
-	return os.Getenv("REALM")
-}
---
+++ backend/config/token.go
@@ -0,0 +1,31 @@
+package config
+
+import "os"
+
+func SecretKeyAccessToken() []byte {
+	return []byte(os.Getenv("SECRET_KEY_ACCESS_TOKEN"))
+}
+
+func SecretKeyRememberMeToken() []byte {
+	return []byte(os.Getenv("SECRET_KEY_REMEMBER_ME_TOKEN"))
+}
+
+func CookieNameAccessToken() string {
+	return os.Getenv("COOKIE_NAME_ACCESS_TOKEN")
+}
+
+func CookieNameRememberMeToken() string {
+	return os.Getenv("COOKIE_NAME_REMEMBER_ME_TOKEN")
+}
+
+func IdentityKeyAccessToken() string {
+	return os.Getenv("KEY_ACCESS_TOKEN")
+}
+
+func IdentityKeyRememberMeToken() string {
+	return os.Getenv("KEY_REMEMBER_ME_TOKEN")
+}
+
+func Realm() string {
+	return os.Getenv("REALM")
+}
