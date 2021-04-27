ed06cd2755907fd26a1eafa4a5ab23c8aa1b5b6a
tarao1006 <taiga4562@gmail.com>
2021-04-28 01:07:54 +0900

    Add remember_me_session

---
+++ db/cheme_reservation_system/remember_me_session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `remember_me_session` (
+  `user_id` VARCHAR(255) NOT NULL,
+  `id` VARCHAR(255) NOT NULL,
+  `expire_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`user_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
