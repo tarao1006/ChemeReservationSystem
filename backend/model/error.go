105bf9857f664f268911885855b882672f199686
tarao1006 <taiga4562@gmail.com>
2021-04-27 16:48:57 +0900

    Update error

--- backend/model/error.go
+++ backend/model/error.go
@@ -4,5 +4,6 @@ import "errors"
 
 var (
 	ErrInvalidUserID            = errors.New("user id is invalid")
+	ErrInvalidPassword          = errors.New("password is invalid")
 	ErrNewPasswordMustBeChanged = errors.New("new password must be changed")
 )
