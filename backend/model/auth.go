4763707d8f5620eeee1fd495ae73e77cc11deb63
tarao1006 <taiga4562@gmail.com>
2021-04-29 17:07:12 +0900

    Remove required

--- backend/model/auth.go
+++ backend/model/auth.go
@@ -3,7 +3,7 @@ package model
 type Auth struct {
 	ID         string `json:"id" binding:"required"`
 	Password   string `json:"password" binding:"required"`
-	RememberMe bool   `json:"remember_me" binding:"required"`
+	RememberMe bool   `json:"remember_me"`
 }
 
 type Password struct {
