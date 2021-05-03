b2271e1bc0a8c53be606e9d3cb9f26d331088616
tarao1006 <taiga4562@gmail.com>
2021-05-03 19:52:58 +0900

    Add time zone label

--- frontend/src/components/WeeklyCalendar/HeadLeftPanel.tsx
+++ frontend/src/components/WeeklyCalendar/HeadLeftPanel.tsx
@@ -14,6 +14,37 @@ const useStyles = makeStyles((theme: Theme) =>
       flex: 1,
       marginLeft: '1px',
     },
+    table: {
+      minWidth: '56px',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      display: 'table',
+    },
+    label: {
+      zIndex: 1,
+      top: '84px',
+      boxSizing: 'border-box',
+      display: 'table-cell',
+      lineHeight: '24px',
+      minHeight: '24px',
+      overflow: 'hidden',
+      position: 'relative',
+      textAlign: 'center',
+      verticalAlign: 'middle',
+      backgroundColor: '#fafafa',
+    },
+    timeZone: {
+      color: '#70757a',
+      fontSize: '10px',
+      height: '20px',
+      lineHeight: '20px',
+      marginLeft: 'auto',
+      maxWidth: '80px',
+      minWidth: '40px',
+      overflow: 'hidden',
+      textAlign: 'right',
+    },
   }),
 )
 
@@ -23,6 +54,13 @@ export const HeadLeftPanel = () => {
   return (
     <div className={classes.root}>
       <div className={classes.main} />
+      <div className={classes.table}>
+        <div className={classes.label}>
+          <div className={classes.timeZone}>
+            GMT+09
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
